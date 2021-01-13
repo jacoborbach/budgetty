d23965417d03bb146897f772e44f028ea16d35b2
jacoborbach <jacoborbach96@gmail.com>
2021-01-13 16:22:13 -0500

    first

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,12 @@
+const initalState =
+{
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+function reducer(state = initalState, action) {
+    return state;
+}
+
+export default reducer;
